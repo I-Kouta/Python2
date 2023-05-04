0512cbadbd47bffbcf9cef3530952f11d7f16181
I-Kouta <kouta.baseball0222@gmail.com>
2023-05-04 13:02:19 +0900

    表示の変更

--- note.py
+++ note.py
@@ -13,10 +13,10 @@ del expr
 
 expr = (x - 3) ** 2
 expr_ex = sympy.expand(expr)
-print(f"{expr} を展開：{expr_ex}")
+print(f"{expr} = {expr_ex}")
 
 expr_factor = sympy.factor(expr_ex)
-print(f"{expr_ex} を因数分解：{expr_factor}")
+print(f"{expr_ex} = {expr_factor}")
 print(f"{expr_ex}=0の解:{sympy.solve(expr_ex)}")
 
 expr1 = x + y - 7
@@ -25,8 +25,8 @@ print(sympy.solve([expr1, expr2]))  # {x: ?, y: ?}
 
 expr3 = x**3 + 2*x**2 - x + 2
 expr3_1 = sympy.diff(expr3)
-print(f"{expr3}を微分：{expr3_1}")
-print(f"{expr3_1}を積分：{sympy.integrate(expr3_1)}", "\n")
+print(f"({expr3})' = {expr3_1}")
+print(f"∮({expr3_1}) = {sympy.integrate(expr3_1)}", "\n")
 
 print(sympy.diff(sin(x)))  # cos(x)
 print(sympy.diff(cos(x)))  # -sin(x)
