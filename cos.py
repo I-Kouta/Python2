7dcfa7232f275c484f7c161456b29ffc759f0000
I-Kouta <kouta.baseball0222@gmail.com>
2023-05-09 14:27:56 +0900

    spaCyライブラリを使用した、en_core_web_smモデルで文書を形態素解析したベクトル化分析

--- cos.py
+++ cos.py
@@ -1,1 +1,12 @@
 # コサイン類似度
+import spacy
+nlp = spacy.load('en_core_web_sm')
+doc = nlp("この文書を解析します")
+for sent in doc.sents:
+    for token in sent:
+        print(
+            token.i,
+            token.text,
+            token.vector,
+            token.vector.shape,
+        )
