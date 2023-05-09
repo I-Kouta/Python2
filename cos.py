0804c5278c7dd1ba17270cad520320c61ac8b69e
I-Kouta <kouta.baseball0222@gmail.com>
2023-05-09 14:55:21 +0900

    コサイン類似度を求める

--- cos.py
+++ cos.py
@@ -1,12 +1,10 @@
 # コサイン類似度
 import spacy
 nlp = spacy.load('en_core_web_sm')
-doc = nlp("この文書を解析します")
-for sent in doc.sents:
-    for token in sent:
-        print(
-            token.i,
-            token.text,
-            token.vector,
-            token.vector.shape,
-        )
+doc1 = nlp("ホントウのキミを知りたいの")
+doc2 = nlp("ホントウのキミが知りたいの")
+doc3 = nlp("僕らはそうマワルカガミ")
+
+print(doc1. similarity(doc2))
+print(doc2. similarity(doc3))
+print(doc3. similarity(doc1))
