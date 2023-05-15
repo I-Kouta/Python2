0528db2e0e733ae7e4fae7c81b66d9639ba69931
I-Kouta <kouta.baseball0222@gmail.com>
2023-05-15 23:01:48 +0900

    二次関数の平方完成と曲線を描かせる

--- function.py
+++ function.py
@@ -4,5 +4,5 @@ from sympy import Symbol, integrate
 
 # 関数の定義
 x = Symbol("x")
-f1 = 2*x**2 + 4*x + 3  # 2(x + 1)**2 + 1
-f2 = x**2 + 3*x + 5  # (x + 3/2)**2 + 11/4
+f1 = x**2 - 6*x - 5
+f2 = -x**2 + 3*x - 2
--- plot.py
+++ plot.py
@@ -2,11 +2,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 x = np.linspace(-10, 10, 100)  # xの範囲を設定
-y = 2*x**2 + 4*x + 3  # 二次曲線の式
+y1 = x**2 - 6*x - 5  # 二次曲線の式1:(x - 3)**2 - 14
+y2 = -x**2 + 3*x - 2  # 二次曲線の式2:-(x - 3/2)**2 + 1/4
 
-plt.plot(x, y)
+plt.plot(x, y1, label='Quadratic Curve 1')
+plt.plot(x, y2, label='Quadratic Curve 2')
 plt.xlabel('x')
 plt.ylabel('y')
 plt.title('Quadratic Curve')
+plt.legend()
 plt.grid(True)
 plt.show()
