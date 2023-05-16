72dab4809df0ec0b4acfcb50d36e1fdc1a167332
I-Kouta <kouta.baseball0222@gmail.com>
2023-05-16 14:53:41 +0900

    交点と面積を計算する

--- function.py
+++ function.py
@@ -9,14 +9,18 @@ f2 = -x**2 + 1
 
 
 def enclosed_area(a, b):
+    # 交点を求める
+    intersection_points = solve(f1 - f2, x)
+    intersection_points = [float(point) for point in intersection_points]
+    # 交点を範囲に反映
+    if a < intersection_points[0] < b:
+        a = intersection_points[0]
+    if a < intersection_points[1] < b:
+        b = intersection_points[1]
+
     area = integrate(f2 - f1, (x, a, b))
     return area
 
 
-# 面積の計算
-a = 1
-b = -1
-# グラフの交点
-intersection_points = solve(f1 - f2, x)
-print(f"交点:{intersection_points}")
-print(f"面積:{abs(enclosed_area(intersection_points))}")
+print(f"交点(x座標):{solve(f1 - f2, x)}")
+print(f"面積:{abs(enclosed_area(-1, 1))}")
