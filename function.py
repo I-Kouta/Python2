714f34bfc85993c0fea1795d4eb1914dcf2be751
I-Kouta <kouta.baseball0222@gmail.com>
2023-05-16 18:57:45 +0900

    面積と座標を返す

--- function.py
+++ function.py
@@ -8,19 +8,14 @@ f1 = x**2 - 1
 f2 = -x**2 + 1
 
 
-def enclosed_area(a, b):
+def enclosed_area():
     # 交点を求める
     intersection_points = solve(f1 - f2, x)
     intersection_points = [float(point) for point in intersection_points]
-    # 交点を範囲に反映
-    if a < intersection_points[0] < b:
-        a = intersection_points[0]
-    if a < intersection_points[1] < b:
-        b = intersection_points[1]
-
-    area = integrate(f2 - f1, (x, a, b))
+    # 面積の計算
+    area = integrate(f2 - f1, (x, intersection_points[0], intersection_points[1]))
     return area
 
 
 print(f"交点(x座標):{solve(f1 - f2, x)}")
-print(f"面積:{abs(enclosed_area(-1, 1))}")
+print(f"面積:{abs(enclosed_area())}")
