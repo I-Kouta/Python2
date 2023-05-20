b7ab10c0e370da6651a67a553eda900410d6bb43
I-Kouta <kouta.baseball0222@gmail.com>
2023-05-20 18:59:33 +0900

    値の範囲とグラフの軸の範囲の変更

--- correlation.py
+++ correlation.py
@@ -3,14 +3,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 n = 100
-x = np.random.rand(n) # 座表のデータ
-y = x + np.random.rand(n) * 0.5 # y座標のデータ(xとの相関がある)
+x = np.random.uniform(1, 10, n) # 座表のデータ
+y = np.random.uniform(0, 100, n) # y座標のデータ(xとの相関がある)
 correlation = np.corrcoef(x, y)[0, 1]
 # グラフ描写
 plt.scatter(x, y, color="b", label="Data")
 plt.title(f"Correlation: {correlation:.2f}")
-plt.xlabel('X')
-plt.ylabel('Y')
+plt.xlim(0, 12)
+plt.ylim(0, 100)
+plt.xlabel('hours of study per day')
+plt.ylabel('result')
 plt.legend()
 plt.grid(True)
 plt.show()
