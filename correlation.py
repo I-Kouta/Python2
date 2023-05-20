a8caf44c6cbd1a0e179e059b01fe656544e2bf4e
I-Kouta <kouta.baseball0222@gmail.com>
2023-05-20 18:22:39 +0900

    相関図の散布図の作成

--- correlation.py
+++ correlation.py
@@ -1,1 +1,16 @@
 # 相関関係のグラフ作成
+import numpy as np
+import matplotlib.pyplot as plt
+
+n = 100
+x = np.random.rand(n) # 座表のデータ
+y = x + np.random.rand(n) * 0.5 # y座標のデータ(xとの相関がある)
+correlation = np.corrcoef(x, y)[0, 1]
+# グラフ描写
+plt.scatter(x, y, color="b", label="Data")
+plt.title(f"Correlation: {correlation:.2f}")
+plt.xlabel('X')
+plt.ylabel('Y')
+plt.legend()
+plt.grid(True)
+plt.show()
