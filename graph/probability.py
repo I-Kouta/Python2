4d19e4b1d18e2bd0761d734b08ebebbd3177b06c
I-Kouta <kouta.baseball0222@gmail.com>
2023-05-21 22:20:36 +0900

    試行回数と確率のグラフ

--- graph/probability.py
+++ graph/probability.py
@@ -3,11 +3,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # 試行条件
-num_trials = 100 # 試行回数
+num_trials = 500 # 試行回数
+hitPer = 1 # パーセント表記
 # 確率計算
+x = np.arange(1, num_trials + 1)
+y = 1 - (1 - hitPer/100) ** x
 
 # グラフ作成
-plt.plot(range(num_trials), probabilities)
+plt.plot(x, y)
 plt.xlabel('number of trials') # 試行回数
 plt.ylabel('result') # 結果
 plt.ylim(0, 1)
