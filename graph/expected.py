2d34823c8ff2e20f0c8f4683074a7128c291faac
I-Kouta <kouta.baseball0222@gmail.com>
2023-05-23 14:50:15 +0900

    値を変数に代入

--- graph/expected.py
+++ graph/expected.py
@@ -6,7 +6,9 @@ def calculate_expected_value(rolls):
     return np.mean(rolls)
 
 num_trials = 1000 # 試行回数
-rolls = np.random.randint(1, 7, size=num_trials) # 1から6までの値を生成
+numBegin = 1
+numEnd = 7 # この値から1引いた値からランダムで値を生成
+rolls = np.random.randint(numBegin, numEnd, size=num_trials) # 1から6までの値を生成
 
 expected_values = []
 for i in range(1, num_trials + 1):
@@ -17,5 +19,6 @@ plt.plot(range(1, num_trials + 1), expected_values)
 plt.axhline(y=3.5, color="r", linestyle="--", label="Expected Value: 3.5")
 plt.xlabel('number of trials')
 plt.ylabel('expected value')
+plt.ylim(1, 6)
 plt.legend()
 plt.show()
