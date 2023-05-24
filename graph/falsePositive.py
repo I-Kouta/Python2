1907cdba1d31fd71fcbe8112f672c68383f7815e
I-Kouta <kouta.baseball0222@gmail.com>
2023-05-24 14:46:31 +0900

    偽陽性についてグラフ(未完)

--- graph/falsePositive.py
+++ graph/falsePositive.py
@@ -1,3 +1,19 @@
 # 偽陽性の割合のグラフ(python falsePositive.py)
 import numpy as np
 import matplotlib.pyplot as plt
+
+def calculate_false_positive_rate(positive_rate, specificity):
+    return 1 - specificity
+
+# 陽性率と特異度の範囲指定
+positive_rates = np.linspace(0.01, 0.5, 100) # 陽性率
+specificities = np.linspace(0.5, 0.99, 100) # 特異度
+# 偽陽性の割合計算
+false_positive_rates = calculate_false_positive_rate(positive_rates, 1 - specificities)
+# グラフ作成
+plt.plot(positive_rates, false_positive_rates)
+plt.xlabel("positive rate")
+plt.ylabel("false positive rate")
+plt.title("False Positive Rate vs Positive Rate")
+plt.grid(True)
+plt.show()
