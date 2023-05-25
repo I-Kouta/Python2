0e71cdf3e359704ab4bab13595a9c3ae7c06d918
I-Kouta <kouta.baseball0222@gmail.com>
2023-05-25 14:45:05 +0900

    偽陽性率と陽性者数のグラフ

--- graph/falsePositive1.py
+++ graph/falsePositive1.py
@@ -1,25 +1,22 @@
 # 偽陽性と特異度のグラフ(python falsePositive1.py)
-# 陽性率は0.01~0.4程度で指定する
+# 陽性者率は0.01~0.4程度で指定する
 import numpy as np
 import matplotlib.pyplot as plt
 
-def calculate_false_positive_rate(positive_rate, specificity):
-    return 1 - specificity
-
-# 陽性率と特異度の範囲指定
+# 陽性者率と特異度の範囲指定
 n = 100000 # サンプル数
-positive_rates = np.linspace(0.01, 0.4, 100) # 陽性率
-# specificities = np.linspace(0.5, 0.99, 100) # 特異度
+positive_rates = np.linspace(0.01, 0.4, 100) # 陽性者率
+specificities = 0.9 # 特異度
 
-# 偽陽性の割合計算
-# 真陽性の数
-# 偽陽性の数
-# 偽陽性の割合
-# false_positive_rates = calculate_false_positive_rate(positive_rates, 1 - specificities)
+# 偽陽性率算出のための情報
+truePositive = n * positive_rates * specificities # 真陽性の数
+falsePositive = n * (1 - positive_rates) * (1 - specificities) # 偽陽性の数
+falsePositiveRates = (falsePositive) / (falsePositive + truePositive) # 偽陽性の割合
+
 # グラフ作成
-plt.plot(positive_rates, false_positive_rates)
-plt.xlabel("positive rate")
-plt.ylabel("false positive rate")
+plt.plot(positive_rates, falsePositiveRates)
+plt.xlabel("positive rate") # 陽性者率
+plt.ylabel("false positive rate") # 偽陽性率
 plt.title("False Positive Rate vs Positive Rate")
 plt.grid(True)
 plt.show()
