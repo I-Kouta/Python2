9a06646261f0e3aa29904ec71a749e44d2c7c2b5
I-Kouta <kouta.baseball0222@gmail.com>
2023-05-25 22:12:24 +0900

    変数名の変更

--- graph/falsePositive1.py
+++ graph/falsePositive1.py
@@ -5,16 +5,16 @@ import matplotlib.pyplot as plt
 
 # 陽性者率と特異度の範囲指定
 n = 100000 # サンプル数
-positive_rates = np.linspace(0.01, 0.4, 100) # 陽性者率
-specificities = 0.9 # 特異度
+positive_rates_x = np.linspace(0.01, 0.4) # 陽性者率(x軸)
+specificities = 0.7 # 特異度
 
 # 偽陽性率算出のための情報
-truePositive = n * positive_rates * specificities # 真陽性の数
-falsePositive = n * (1 - positive_rates) * (1 - specificities) # 偽陽性の数
+truePositive = n * positive_rates_x * specificities # 真陽性の数
+falsePositive = n * (1 - positive_rates_x) * (1 - specificities) # 偽陽性の数
 falsePositiveRates = (falsePositive) / (falsePositive + truePositive) # 偽陽性の割合
 
 # グラフ作成
-plt.plot(positive_rates, falsePositiveRates)
+plt.plot(positive_rates_x, falsePositiveRates)
 plt.xlabel("positive rate") # 陽性者率
 plt.ylabel("false positive rate") # 偽陽性率
 plt.title(f"positive rate - false positive rate (specificities:{specificities * 100}%)")
