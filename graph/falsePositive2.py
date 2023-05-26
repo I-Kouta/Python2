96349f959e20c3918d2a7ecfcf8448551cd41361
I-Kouta <kouta.baseball0222@gmail.com>
2023-05-26 14:32:03 +0900

    Revert "特異度と陽性者数のグラフ"
    This reverts commit 0bc3e0f8daec3c4a3a31d51ae50a0b81ef06ea99.

--- graph/falsePositive2.py
+++ graph/falsePositive2.py
@@ -2,18 +2,3 @@
 # 特異度は0.5~0.9程度で指定する
 import numpy as np
 import matplotlib.pyplot as plt
-
-n = 100000 # サンプル数
-positive_rates = 0.01 # 陽性者率
-specificities_x = np.linspace(0.55, 0.9) # 特異度(x軸)
-
-truePositive = n * positive_rates * specificities_x # 真陽性の数
-falsePositive = n * (1 - positive_rates) * (1 - specificities_x) # 偽陽性の数
-falsePositiveRates = (falsePositive) / (falsePositive + truePositive) # 偽陽性の割合
-
-plt.plot(specificities_x, falsePositiveRates)
-plt.xlabel("specificities") # 特異度
-plt.ylabel("false positive rate") # 偽陽性率
-plt.title(f"positive rate - specificities (positive rates:{positive_rates * 100}%)") # 陽性者率を変数で表示させる
-plt.grid(True)
-plt.show()
