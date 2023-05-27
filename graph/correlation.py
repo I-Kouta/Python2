ed7fc40c0f89777cab1739263f17d781c7e88cbd
I-Kouta <kouta.baseball0222@gmail.com>
2023-05-27 13:03:47 +0900

    x,yに相関係数を反映させる記述

--- graph/correlation.py
+++ graph/correlation.py
@@ -3,12 +3,20 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 n = 100
+targetCorrelation = 0.8 # 相関係数を指定
+
 x = np.random.uniform(1, 10, n) # 座表のデータ
-y = np.random.uniform(0, 100, n) # y座標のデータ(xとの相関がある)
-correlation = np.corrcoef(x, y)[0, 1]
+targetCovariance = np.array([[1, targetCorrelation], [targetCorrelation, 1]]) # yを生成する相関係数行列
+# 共分散行列の固有値分解を行い、変換行列を取得
+eigenvalues, eigenvectors = np.linalg.eig(targetCovariance)
+transformationMatrix = eigenvectors @ np.diag(np.sqrt(eigenvalues))
+# 相関が反映されたyを生成
+y = np.random.randn(n, 2) @ transformationMatrix
+y = np.interp(y[:, 1], (y[:, 1].min(), y[:, 1].max()), (1, 10))
+# correlation = np.corrcoef(x, y)[0, 1]
 # グラフ描写
 plt.scatter(x, y, color="b", label="Data")
-plt.title(f"Correlation: {correlation:.2f}")
+plt.title(f"Correlation: {targetCorrelation}")
 plt.xlim(0, 12)
 plt.ylim(0, 100)
 plt.xlabel('hours of study per day')
