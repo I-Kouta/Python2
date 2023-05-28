914778e62ada46f2b70a274a22117a2e630d6ae9
I-Kouta <kouta.baseball0222@gmail.com>
2023-05-28 21:44:50 +0900

    ライブラリと初期費用、金利の設定

--- graph/interest_rate.py
+++ graph/interest_rate.py
@@ -1,1 +1,7 @@
-# 複利計算(interest_rate.py)
+# 複利計算(python interest_rate.py)
+import numpy as np
+import matplotlib.pyplot as plt
+# 90年12月末:6.08%
+deposit = 10000
+pastRate = 0.0575 # 70年12月末:5.75%
+recentlyRate = 0.00003 # 22年3月末:0.003%
