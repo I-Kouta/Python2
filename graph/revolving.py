a9d239d14af90b3da708db4bdcbe654b5b3f5b1f
I-Kouta <kouta.baseball0222@gmail.com>
2023-05-30 08:51:49 +0900

    リボ払いシミュレーションのグラフ chatGPTの回答

--- graph/revolving.py
+++ graph/revolving.py
@@ -1,8 +1,25 @@
-# リボ払いシミュレーション
+# リボ払いシミュレーション(python revolving.py)
 import numpy as np
 import matplotlib.pyplot as plt
+
+def calculate_balance(principal, monthlyInterestRate, monthlyPayment):
+    balances = [principal]
+    for _ in range(12):
+        interest = balances[-1] * monthlyInterestRate
+        balance = balances[-1] + interest - monthlyPayment
+        balances.append(balance)
+    return balances
 
 principal = 100000 # 初期費用10万円
 interestRate = 15
 monthlyInterestRate = interestRate / 12
 monthlyPayment = 20000 # 月の支払い金額
+
+balances = calculate_balance(principal, monthlyInterestRate, monthlyPayment)
+months = np.arange(13)
+
+plt.plot(months, balances)
+plt.xlabel("months")
+plt.ylabel("balances")
+plt.grid(True)
+plt.show()
