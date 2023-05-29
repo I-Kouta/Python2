46646d38c1ea5b08a764f9c2d57ee931c38a3297
I-Kouta <kouta.baseball0222@gmail.com>
2023-05-29 22:55:05 +0900

    新たなファイルの作成と一部変数の定義

---
+++ graph/revolving.py
@@ -0,0 +1,8 @@
+# リボ払いシミュレーション
+import numpy as np
+import matplotlib.pyplot as plt
+
+principal = 100000 # 初期費用10万円
+interestRate = 15
+monthlyInterestRate = interestRate / 12
+monthlyPayment = 20000 # 月の支払い金額
