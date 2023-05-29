a96291f8c8391dbee7c4b51a1d18e932ee3acb57
I-Kouta <kouta.baseball0222@gmail.com>
2023-05-29 14:50:48 +0900

    グラフを複数本書く記述

--- graph/interest_rate.py
+++ graph/interest_rate.py
@@ -4,21 +4,24 @@ import matplotlib.pyplot as plt
 # 90年12月末:6.08%
 
 def calculate_deposit(deposit, pastRate, years):
-    deposit = [deposit]
+    deposit_values = [deposit]
     for _ in range(years):
-        deposit.append(deposit[-1] * (1 + pastRate))
-    return deposit
+        deposit_values.append(deposit_values[-1] * (1 + pastRate))
+    return deposit_values
 
 deposit = 1000000 # 100万
 pastRate = 0.0575 # 70年12月末:5.75%
 recentlyRate = 0.00003 # 22年3月末:0.003%
-years = 10
-depositValues = calculate_deposit(deposit, pastRate, years)
+years = 100
 
 yearsRange = np.arange(years + 1)
-plt.plot(yearsRange, depositValues)
+pastRateValues = calculate_deposit(deposit, pastRate, years) # 過去の利率のグラフ
+recentlyValues = calculate_deposit(deposit, recentlyRate, years) # 最近の利率のグラフ
+
+plt.plot(yearsRange, pastRateValues, label=f"interest rate: {pastRate * 100}%")
+plt.plot(yearsRange, recentlyValues, label=f"interest rate: {recentlyRate * 100}%")
 plt.xlabel("years")
 plt.ylabel("deposit amount")
-plt.title(f"deposit rate - {pastRate * 100}%")
+plt.legend()
 plt.grid(True)
 plt.show()
