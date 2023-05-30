ed615e227ec5e297cf2a3b6415d0bf922d429c85
I-Kouta <kouta.baseball0222@gmail.com>
2023-05-30 19:46:53 +0900

    データの評価を行う記述

--- Scikit-learn/data.py
+++ Scikit-learn/data.py
@@ -1,4 +1,24 @@
-# 機械学習ライブラリの使い方
-from sklearn .<algorithm> import <model>
-import numpy as np
-import pandas as pd
+# 機械学習ライブラリの使い方(python data.py)
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+
+# データのロード
+iris = datasets.load_iris()
+X = iris.data
+y = iris.target
+
+# データの分割
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# モデルの作成とトレーニング
+model = LogisticRegression()
+model.fit(X_train, y_train)
+
+# 予測の実行
+y_pred = model.predict(X_test)
+
+# 精度の評価
+accuracy = accuracy_score(y_test, y_pred)
+print("Accuracy:", accuracy)
