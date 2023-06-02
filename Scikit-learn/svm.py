6424cbda9b3c5880d5dfdac33606e58a5048b06f
I-Kouta <kouta.baseball0222@gmail.com>
2023-06-02 20:05:48 +0900

    メモ追記

--- Scikit-learn/svm.py
+++ Scikit-learn/svm.py
@@ -1,3 +1,4 @@
+# python svm.py
 from sklearn import svm, metrics
 
 xor_data = [
@@ -13,14 +14,14 @@ for row in xor_data:
     p = row[0]
     q = row[1]
     r = row[2]
-    data.append([p, q])
-    label.append(r)
+    data.append([p, q]) # 学習データを格納
+    label.append(r) # labelに答えを格納,fit()メソッドで学習
 
-clf = svm.SVC()
-clf.fit(data, label)
-pre = clf.predict(data)
+clf = svm.SVC() # SVMオブジェクトを生成
+clf.fit(data, label) # 与えられたデータを学習
+pre = clf.predict(data) # 学習結果からデータを予測
 print('予測：', pre)
-score = metrics.accuracy_score(label, pre)
+score = metrics.accuracy_score(label, pre) # 正解率を求める
 print('正解率：', score)
 # 予測： [0 1 1 1]
 # 正解率： 1.0
