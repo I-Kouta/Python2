a7dcc01e4ce42b7115656e79ad1bb9b54d54343c
I-Kouta <kouta.baseball0222@gmail.com>
2023-06-04 19:26:55 +0900

    ライブラリのデータ読み込み

--- Scikit-learn/cart.py
+++ Scikit-learn/cart.py
@@ -1,1 +1,7 @@
 # 決定木のアルゴリズム,予測分析(cart.py)
+# ライブラリの読み込み
+from sklearn.tree import DecisionTreeClassifier
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+
+iris = datasets.load_iris() # データ読み込み
