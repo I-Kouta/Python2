1d20338875ee89bf471d238a14ac7adc22ed3789
I-Kouta <kouta.baseball0222@gmail.com>
2023-06-04 22:10:02 +0900

    ファイル名と内容変更

---
+++ Scikit-learn/randomForest.py
@@ -0,0 +1,9 @@
+# ランダムフォレスト実装(randomForest.py)
+# ライブラリの読み込み
+import glob
+import pandas as pd
+import numpy as np
+import category_encoders as ce
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import r2_score
