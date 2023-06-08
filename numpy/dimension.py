c58a846d91f4c7ab58d332beb99c560d6b03374a
I-Kouta <kouta.baseball0222@gmail.com>
2023-06-08 22:25:24 +0900

    3次元図の表示

--- numpy/dimension.py
+++ numpy/dimension.py
@@ -8,3 +8,20 @@ x = wine.data[:, :3] # 3次元のデータを読み込む
 y = wine.target # 分類ラベル
 feature_name = wine.feature_names[:3] # 特微量の名前
 target_name = wine.target_names # 目的量の名前
+
+fig = plt.figure()
+ax = fig.add_subplot(projection="3d") # 3Dで投影
+colors = ["red", "blue", "green"]
+for yy in np.unique(y):
+    ax.scatter(
+        x[:,0][y==yy],
+        x[:,1][y==yy],
+        x[:,2][y==yy],
+        label=target_name[yy],
+        color=colors[yy],
+        edgecolors='black')
+ax.set_xlabel(feature_name[0]) # alcohol:アルコール
+ax.set_ylabel(feature_name[1]) # malic_acid:リンゴ酸
+ax.set_zlabel(feature_name[2]) # ash:灰分
+plt.legend()
+plt.show()
