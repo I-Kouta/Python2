cd86be848691df96b1017fa397b60661dd263b92
I-Kouta <kouta.baseball0222@gmail.com>
2023-06-08 14:45:24 +0900

    3次元の図のプロットデータの記述

--- numpy/dimension.py
+++ numpy/dimension.py
@@ -2,3 +2,9 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.datasets import load_wine
+
+wine = load_wine() # データセットをロード
+x = wine.data[:, :3] # 3次元のデータを読み込む
+y = wine.target # 分類ラベル
+feature_name = wine.feature_names[:3] # 特微量の名前
+target_name = wine.target_names # 目的量の名前
