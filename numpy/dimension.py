ab1cae7e331925b541de969f22278a137d903379
I-Kouta <kouta.baseball0222@gmail.com>
2023-06-10 11:55:48 +0900

    特微量の指定方法の修正

--- numpy/dimension.py
+++ numpy/dimension.py
@@ -4,7 +4,7 @@ import numpy as np
 from sklearn.datasets import load_wine
 
 wine = load_wine() # データセットをロード
-x = wine.data[:, :3] # 3次元のデータを読み込む
+x = wine.data[:, [0, 1, 2]] # 3次元のデータを読み込む
 y = wine.target # 分類ラベル
 feature_name = wine.feature_names[:3] # 特微量の名前
 target_name = wine.target_names # 目的量の名前
