55e532411f28f83b6c0b023793f28a26831adbc6
I-Kouta <kouta.baseball0222@gmail.com>
2023-06-14 23:24:16 +0900

    シグモイド関数での定義の準備

--- numpy/classifying.py
+++ numpy/classifying.py
@@ -5,10 +5,17 @@ import matplotlib.pyplot as plt
 x = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20])
 # y:対応する正解ラベル(0:正常, 1:異常)
 y = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1])
+# パラメータ設定
+a = 0
+b = 0
+# 仮説を定義
+z = a * x + b # 線形回帰の仮説
+h = 1 / (1 + np.exp(-z)) # シグモイド関数
 
-plt.yticks([0,0.5,1]) #y軸の目盛り
-plt.scatter(x[:10] ,y[:10], c='b')
-plt.scatter(x[10:] ,y[10:], c='r')
+plt.yticks([0, 1]) #y軸の目盛り
+plt.scatter(x, y)
+plt.plot(x, h, c="orange")
+plt.legend((u"data", u"regression line")) # 凡例
 plt.xlabel('Number of defective pridcts')
 plt.ylabel('Label')
 plt.show()
