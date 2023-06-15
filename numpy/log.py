9fce369138f0bdf822dcfe323444bf50d9c4b2e5
I-Kouta <kouta.baseball0222@gmail.com>
2023-06-15 19:27:23 +0900

    不要な記述と値の最適化

--- numpy/log.py
+++ numpy/log.py
@@ -1,12 +1,11 @@
 # 対数関数(python log.py)
 import numpy as np
-from sympy import Symbol, log
 from sympy.plotting import plot
 import matplotlib.pyplot as plt
 # 関数の定義
 def log10(x):
     return np.log10(x)
-x_values = np.linspace(0.1, 1, 100)
+x_values = np.linspace(0, 1, 1000)
 f1 = -log10(x_values)
 f2 = -log10(1 - x_values)
 # グラフ表示
