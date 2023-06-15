9144f665af2cb3727f244d8bd82eeef5e3c3a98f
I-Kouta <kouta.baseball0222@gmail.com>
2023-06-15 14:49:07 +0900

    対数関数のグラフ表示

--- numpy/log.py
+++ numpy/log.py
@@ -1,4 +1,17 @@
-# 対数関数(log.py)
-from sympy import Symbol, integrate, solve
+# 対数関数(python log.py)
+import numpy as np
+from sympy import Symbol, log
 from sympy.plotting import plot
-from fractions import Fraction
+import matplotlib.pyplot as plt
+# 関数の定義
+def log10(x):
+    return np.log10(x)
+x_values = np.linspace(0.1, 1, 100)
+f1 = -log10(x_values)
+f2 = -log10(1 - x_values)
+# グラフ表示
+plt.plot(x_values, f1, label='f1 = -log(X)')
+plt.plot(x_values, f2, label='f2 = -log(1 - X)')
+plt.legend()
+plt.grid()
+plt.show()
