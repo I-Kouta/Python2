a71b0b13ed88db3d4bd0e884e51131d247be73ca
I-Kouta <kouta.baseball0222@gmail.com>
2023-06-17 12:35:39 +0900

    ノード3つの木の作成

--- inference/dfs.py
+++ inference/dfs.py
@@ -1,1 +1,9 @@
 # 深さ優先探索(python dfs.py)
+# 木構造を表すデータ構造のためのクラス
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+root = TreeNode(1, 2, 3) # 木構造を作成
+print(root.val, root.left, root.right)
