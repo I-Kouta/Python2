10fedd0d2a1114d0f10728619886063ea098c06f
I-Kouta <kouta.baseball0222@gmail.com>
2023-06-18 16:21:59 +0900

    深さ優先探索の実装

--- inference/dfs.py
+++ inference/dfs.py
@@ -1,12 +1,13 @@
 # 深さ優先探索(python dfs.py)
 # 木構造を表すデータ構造のためのクラス
+from collections import deque
 class TreeNode: # 左下と右下に繋がっているノードが何かを定義している
     def __init__(self, val=0, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
 
-tree = [1, 2, 3, 4, 5, 6, 7] # このノードを左右のノードに登録する
+tree = [1, 2, 3, 4, 5, 6] # このノードを左右のノードに登録する
 
 def make_tree(tree, node, i, n): # 二文木を作成
     # node:今見ているノード
@@ -21,5 +22,17 @@ def make_tree(tree, node, i, n): # 二文木を作成
         node.right = make_tree(tree, node.right, 2 * i + 2, n)
     return node
 
+ans = [] # 答えを入れるリスト
 root = make_tree(tree, None, 0, len(tree)) # スタート地点
-print(root.val, root.left.val, root.right.right.val)
+stack = deque([root]) # データ構造にリストを置換
+# 深さ優先探索の実装, 空になるまでループを続ける
+while stack:
+    node = stack.pop()
+    ans.append(node.val)
+    if node.right:
+        stack.append(node.right)
+    if node.left:
+        stack.append(node.left)
+
+print(root.val, root.left.val, root.right.val, root.left.left.val, root.left.right.val, root.right.left.val)
+print(ans)
