a219c8c6a48ea5e588fb0eb0f6dee6a9f24eebfe
I-Kouta <kouta.baseball0222@gmail.com>
2023-06-28 19:33:50 +0900

    パーセプトロンの記述

--- deep-learning/neutral.py
+++ deep-learning/neutral.py
@@ -1,1 +1,15 @@
 # ニュートラルネットワーク(python neutral.py)
+# パーセプトロン：複数の信号を受け取った時に0, 1どちらかを出力する
+def simple_perceptron(x1, x2):
+    w1 = 0.2 # x1の重み
+    w2 = 0.8 # x2の重み
+    b = -0.7 # 発火しやすさを調整するパラメータ
+    y = x1 * w1 + x2 * w2 + b
+    if y <= 0:
+      return 0
+    elif y > 0:
+      return 1
+
+#いくつか値をいれて、実際にためしてみましょう↓
+print(simple_perceptron(0, 0))
+print(simple_perceptron(0, 1))
