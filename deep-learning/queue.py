e07ad31280bac594f6dc1232475ca649d6aac504
I-Kouta <kouta.baseball0222@gmail.com>
2023-06-29 14:46:58 +0900

    行列の計算のメモ追記

--- deep-learning/queue.py
+++ deep-learning/queue.py
@@ -3,5 +3,7 @@ import numpy as np
 
 matrixA = np.array([[1, 2]])
 matrixB = np.array([[1, 2, 3], [4, 5, 6]])
-
+ # 1 * 1 + 2 * 4 = 9
+ # 1 * 2 + 2 * 5 = 12
+ # 1 * 3 + 2 * 6 = 15
 print(f"行列の積:{np.dot(matrixA,  matrixB)}")
