095999807d7aa7f220e14e1dc6210d47e56a2258
I-Kouta <kouta.baseball0222@gmail.com>
2023-07-03 14:39:18 +0900

    float型に変換

--- PyTorch/XOR.py
+++ PyTorch/XOR.py
@@ -1,1 +1,7 @@
 # XORゲートの実装(python XOR.py)
+import torch
+# tensorデータを作成(多次元配列), int16, float32型など
+x = torch.tensor([8], dtype = torch.float) # intからfloatに変換
+
+print(x)
+print(x.dtype) # tensorに渡したデータの型を表示
