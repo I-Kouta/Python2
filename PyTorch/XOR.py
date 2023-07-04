61b2d5eca934ceb1b161906870b0e775820d0b6c
I-Kouta <kouta.baseball0222@gmail.com>
2023-07-04 20:18:30 +0900

    線形変換した結果を出力させる

--- PyTorch/XOR.py
+++ PyTorch/XOR.py
@@ -1,1 +1,11 @@
 # XORゲートの実装(python XOR.py)
+import torch
+import torch.nn as nn # nnモジュール
+
+linear_model = nn.Linear(1, 1) # 線形モデルのインスタンスを生成, 入力値(tensor型)・出力値(tensor型)・バイアス(bool)
+
+x = torch.tensor([3.0]) # 線形変換した結果の出力が得られる
+
+print(f"重み:{linear_model.weight}", "\n") # 重み
+print(f"バイアス:{linear_model.bias}", "\n") # バイアス
+print(f"tensorを入力として代入した結果:{linear_model(x)}")
