de806b65277cffefdd46b3e5d58431386f940d06
I-Kouta <kouta.baseball0222@gmail.com>
2023-07-05 14:56:37 +0900

    サブモジュールの連結

--- PyTorch/XOR.py
+++ PyTorch/XOR.py
@@ -9,3 +9,10 @@ x = torch.tensor([3.0]) # 線形変換した結果の出力が得られる
 print(f"重み:{linear_model.weight}", "\n") # 重み
 print(f"バイアス:{linear_model.bias}", "\n") # バイアス
 print(f"tensorを入力として代入した結果:{linear_model(x)}")
+
+model = nn.Sequential( # いくつかのサブモジュールをまとめて連結したモデル
+    nn.Linear(2, 1), # 線形層(入力数, 出力数)
+    nn.Sigmoid() # シグモイド関数
+)
+
+print(model)
