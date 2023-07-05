72754d70e30379410aa93ebab1f413fcf78507b4
I-Kouta <kouta.baseball0222@gmail.com>
2023-07-05 22:11:37 +0900

    Sequentialモデルに値を入力して出力を得る(forwardメソッド)

--- PyTorch/example2.py
+++ PyTorch/example2.py
@@ -1,4 +1,4 @@
-# XORゲートの実装(python XOR.py)
+# XORゲートの実装(python example2.py)
 import torch
 import torch.nn as nn # nnモジュール
 
@@ -8,11 +8,16 @@ x = torch.tensor([3.0]) # 線形変換した結果の出力が得られる
 
 print(f"重み:{linear_model.weight}", "\n") # 重み
 print(f"バイアス:{linear_model.bias}", "\n") # バイアス
-print(f"tensorを入力として代入した結果:{linear_model(x)}")
+print(f"tensorを入力として代入した結果:{linear_model(x)}, \n")
 
 model = nn.Sequential( # いくつかのサブモジュールをまとめて連結したモデル
     nn.Linear(2, 1), # 線形層(入力数, 出力数)
     nn.Sigmoid() # シグモイド関数
 )
 
-print(model)
+x = torch.tensor([3.0, 2.0])
+t_p = model.forward(x)
+
+print(model, "\n")
+
+print(t_p) # モデルにテンソルを入力して出力を得る
