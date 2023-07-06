bffc889429a0de9312fecfbc7182d55750ae34e0
I-Kouta <kouta.baseball0222@gmail.com>
2023-07-06 14:54:59 +0900

    ゲインごとのシグモイド関数を表示

--- function/sigmoid.py
+++ function/sigmoid.py
@@ -2,14 +2,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def sigmoid(x): # シグモイド間数の定義
-    return 1 / (1 + np.exp(-x))
+def sigmoid(x, a): # シグモイド間数の定義
+    return 1 / (1 + np.exp(-a * x))
 
 x = np.linspace(-8, 8, 1000)
-y = sigmoid(x)
+a = np.linspace(0.1, 1.0, 10)
 
-fig = plt.figure()
-ax = fig.add_subplot(111)
-ax.plot(x, y)
-plt.grid()
+fig = plt.figure(figsize = (12, 12))
+for i in range(len(a)):
+    text = "a = " + str(a[i])
+    ax = fig.add_subplot(5, 5, i + 1, title = text)
+    ax.plot(x, sigmoid(x, a[i]))
+
+fig.subplots_adjust(wspace=0.5, hspace=0.5)
 plt.show()
