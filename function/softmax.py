1449379402ba4dcfd16fc97eadbc3d631c587210
I-Kouta <kouta.baseball0222@gmail.com>
2023-07-07 20:56:34 +0900

    ソフトマックス関数の記述

--- function/softmax.py
+++ function/softmax.py
@@ -1,2 +1,12 @@
 # ソフトマックス関数(python softmax.py)
-# 出力の値の合計が1になる
+# 出力の値の合計が1になる → 確率として解釈
+import numpy as np
+
+def soft_max(x): # 関数の定義
+    return np.exp(x) / np.sum(np.exp(x))
+
+X = np.array([5, 3, 4]) # 入力値
+y = soft_max(X)
+
+print(y)
+print(np.sum(y))
