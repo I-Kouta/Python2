534e5fa7c21664efcfeb6986cde5a9ed59046ea4
I-Kouta <kouta.baseball0222@gmail.com>
2023-07-08 12:35:32 +0900

    ReLU関数の記述

--- function/ReLU.py
+++ function/ReLU.py
@@ -1,1 +1,15 @@
-# (python ReLU.py)
+# ランプ関数:勾配消失が起こりにくい(python ReLU.py)
+import numpy as np
+import matplotlib.pyplot as plt
+# DeLU関数の定義
+def relu(x):
+    return np.maximum(0, x) # 入力が0以下の場合は0を出力する
+
+x = np.linspace(-8, 8, 1000)
+y = relu(x)
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.plot(x, y)
+plt.grid()
+plt.show()
