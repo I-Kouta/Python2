58d4e9d0efb5717f4d1f5c4de1eb6c63b70697f0
I-Kouta <kouta.baseball0222@gmail.com>
2023-07-10 14:55:40 +0900

    計算と勾配の確認

--- PyTorch/XOR.py
+++ PyTorch/XOR.py
@@ -1,4 +1,5 @@
 # XORゲート(python XOR.py)
+import torch
 import torch.nn as nn
 
 # 3層のニュートラルネットワークを定義
@@ -11,5 +12,14 @@ model = nn.Sequential(
 
 loss_fn = nn.BCELoss() # 真の値と予測を比較して誤差を返す(損失関数)
 
+x = torch.tensor([2.0], requires_grad = True) # 線形変換
+w = torch.tensor([4.0], requires_grad = True) # 重み
+b = torch.tensor([-7.0], requires_grad = True) # バイアス
+
 print(model)
 print(loss_fn)
+
+y = w * x + b
+print(y) # 計算の流れを記憶できる
+y.backward() # 計算の逆向きに自動で勾配を計算してくれる
+print(w.grad)
