423db51fdd9b48946dfdee9599e16f853e5a8717
I-Kouta <kouta.baseball0222@gmail.com>
2023-07-11 19:07:12 +0900

    損失の減少をグラフ化

--- PyTorch/XOR.py
+++ PyTorch/XOR.py
@@ -1,6 +1,7 @@
 # XORゲート(python XOR.py)
 import torch
 import torch.nn as nn
+from matplotlib import pyplot as plt
 
 X = torch.tensor([[0.0, 0.0], [0.0, 1.0], [1.0, 0.0], [1.0, 1.0]], requires_grad = True) # データ
 y = torch.tensor([[0.0], [1.0], [1.0], [0.0]]) # ラベル
@@ -15,13 +16,21 @@ model = nn.Sequential( # 3層のニュートラルネットワークを定義
 loss_fn = nn.BCELoss() # 真の値と予測を比較して誤差を返す(損失関数)
 
 optimizer = torch.optim.SGD(params = model.parameters(), lr = 0.1)
+y_axis_list = [] # y軸方向の値を代入するリスト
 
 for epoch in range(2000): # 2000エポック学習させる
     optimizer.zero_grad() # 勾配を0に初期化する必要がある
     t_p = model.forward(X) # 順伝播
     loss = loss_fn(t_p, y) # 損失を計算
+    y_axis_list.append(loss.detach().numpy())
     loss.backward() # 逆伝播
     optimizer.step() # オプティマイザがパラメータの更新量を計算し、モデルに返してパラメータ更新
+    if epoch % 100 == 0: # 100エポック毎に損失の値を表示
+        print("epoch: %d  loss: %f" % (epoch + 1 ,float(loss)))
 
-if epoch % 100 == 0:#100エポック毎に損失の値を表示
-    print("epoch: %d  loss: %f" % (epoch + 1 ,float(loss)))
+x_axis_list = [num for num in range(2000)]#x軸方向のリスト
+
+plt.xlabel("epoch")
+plt.ylabel("loss")
+plt.plot(x_axis_list,y_axis_list)
+plt.show()
