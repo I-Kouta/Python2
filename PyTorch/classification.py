3f367603d582387f076ad7098623f9f8292c2663
I-Kouta <kouta.baseball0222@gmail.com>
2023-07-11 20:05:08 +0900

    データセットをまとめる

--- PyTorch/classification.py
+++ PyTorch/classification.py
@@ -2,3 +2,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn import datasets
+
+samples = 500
+X, Y = datasets.make_circles(n_samples = samples, shuffle = False, factor = 0.3, noise = 0.1)
+# 形状
+print(X.shape) # データ
+print(Y.shape) # ラベル
