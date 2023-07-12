158326d0f760e8bbb88938d6f061a8e3d09f1baf
I-Kouta <kouta.baseball0222@gmail.com>
2023-07-12 14:46:50 +0900

    データを分割

--- PyTorch/classification.py
+++ PyTorch/classification.py
@@ -2,9 +2,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn import datasets
+from sklearn.model_selection import train_test_split
 
 samples = 500
 X, Y = datasets.make_circles(n_samples = samples, shuffle = False, factor = 0.3, noise = 0.1)
+train_X, test_X, train_Y, test_Y = train_test_split(X, Y, test_size=0.2)
 # 形状
-print(X.shape) # データ
-print(Y.shape) # ラベル
+print(train_X.shape)
+print(test_X.shape)
+print(train_Y.shape)
+print(test_Y.shape)
