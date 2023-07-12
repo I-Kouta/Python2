e3c169c52cbfe905fdc8f1cd1ede6ed656a59383
I-Kouta <kouta.baseball0222@gmail.com>
2023-07-12 21:07:06 +0900

    モデルに学習をさせる準備

--- PyTorch/classification.py
+++ PyTorch/classification.py
@@ -1,12 +1,17 @@
 # 2クラスの色分け(python classification.py)
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
+from torch.utils.data import TensorDataset
+from torch.utils.data import DataLoader
 
 samples = 500
 X, Y = datasets.make_circles(n_samples = samples, shuffle = False, factor = 0.3, noise = 0.1)
-train_X, test_X, train_Y, test_Y = train_test_split(X, Y, test_size=0.2)
+train_X, test_X, train_Y, test_Y = train_test_split(X, Y, test_size=0.2) # シャッフルして分割
 # 形状
 print(train_X.shape)
 print(test_X.shape)
