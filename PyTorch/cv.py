b79d606e7259be0e989de698bbc40498624064e5
I-Kouta <kouta.baseball0222@gmail.com>
2023-07-15 19:23:59 +0900

    画像を左右反転させる

--- PyTorch/cv.py
+++ PyTorch/cv.py
@@ -1,11 +1,29 @@
 # データ拡張(python cv.py)
+import torch
+import torchvision
+import torchvision.transforms as transforms
 import matplotlib.pyplot as plt
+import numpy as np
 import cv2
 # OpenCVを使って画像を読み込む
-img = cv2.imread('/Users/ko-ta/downloads/ダーク系.jpg')
+img = cv2.imread("/Users/ko-ta/downloads/ダーク系.jpg")
 #BGRをRGBに変換
 img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+# 変換
+transform = transforms.Compose([
+    transforms.ToPILImage(),  # PIL画像に変換する処理
+    transforms.RandomHorizontalFlip(p = 1)
+])
+transform = transform(img_rgb)
 
-#読み込んだ画像を表示
-plt.imshow(img_rgb)
-plt.show()
+def visualize(original, reverse):
+    plt.subplot(1, 2, 1)
+    plt.title("original")
+    plt.imshow(original)
+
+    plt.subplot(1, 2, 2)
+    plt.title("reverse")
+    plt.imshow(reverse)
+    plt.show()
+
+visualize(img_rgb, transform)
