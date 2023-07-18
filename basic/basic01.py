79bcdec963242934884f46d83ef13e8a4d8c7670
I-Kouta <kouta.baseball0222@gmail.com>
2023-07-18 19:37:41 +0900

    フィートからインチに変換する

--- basic/basic01.py
+++ basic/basic01.py
@@ -4,3 +4,10 @@ print(math.sqrt(2)) # 平方根の記述
 print(math.pi) # 円周率
 val01 = (math.sqrt(5) + 1) / 2 # 5の平方根に1を加えて2で割る(黄金比)
 print(val01)
+# fフィートiインチをセンチメートルに変換する(1フィート = 12インチ = 30.48cm)
+def ft_to_cm(f, i):
+  return (f + i / 12) * 30.48
+
+feet = 3
+inch = 3
+print(f"{feet}フィート + {inch}インチ = {ft_to_cm(feet, inch)}cm")
