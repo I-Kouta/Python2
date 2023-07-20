8db413996660e0a0bae8ee305fbe40b99e13b470
I-Kouta <kouta.baseball0222@gmail.com>
2023-07-21 08:49:58 +0900

    中央値を求める

--- basic/basic02.py
+++ basic/basic02.py
@@ -3,3 +3,16 @@ def square(x):
     return x ** x
 a = -2
 assert square(a) >= 0 # 条件式が真であることを宣言, 偽ならプログラム停止(AssertionError)
+
+def median(x, y, z): # 中央値を算出する
+  if x > y:
+    w = x
+    x = y
+    y = w # x, yを入れ替えている
+  if x > z:
+    return x
+  if y > z:
+    return z
+  return y
+
+assert median(3, 1, 2) == 2
