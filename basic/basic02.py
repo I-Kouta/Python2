91dce8dc40e3402665ac8f02068658d50fd1bd4c
I-Kouta <kouta.baseball0222@gmail.com>
2023-07-21 19:03:55 +0900

    文字列の検索

--- basic/basic02.py
+++ basic/basic02.py
@@ -5,14 +5,19 @@ a = -2
 assert square(a) >= 0 # 条件式が真であることを宣言, 偽ならプログラム停止(AssertionError)
 
 def median(x, y, z): # 中央値を算出する
-  if x > y:
-    w = x
-    x = y
-    y = w # x, yを入れ替えている
-  if x > z:
-    return x
-  if y > z:
-    return z
-  return y
-
+    if x > y:
+        w = x
+        x = y
+        y = w # x, yを入れ替えている
+    if x > z:
+        return x
+    if y > z:
+        return z
+    return y
 assert median(3, 1, 2) == 2
+
+word1 = "subtitle"
+substr1 = "le"
+substr2 = "me"
+print(substr1 in word1) # true
+print(substr2 in word1) # false
