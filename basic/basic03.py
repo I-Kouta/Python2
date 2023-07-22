5bf967c283c78170956e39b542235b8ab4e2a279
I-Kouta <kouta.baseball0222@gmail.com>
2023-07-22 11:40:38 +0900

    各文字の頻度を求める

--- basic/basic03.py
+++ basic/basic03.py
@@ -1,4 +1,6 @@
 # 条件分岐(python basic03.py)
+import random
+
 def exception3(x, y, z):
     if x == y:
         return z
@@ -10,3 +12,23 @@ def exception3(x, y, z):
         return x + y + z
 
 print(exception3(1, 1, 2))
+
+def random_sequence(length): # ランダム文字列を生成する
+    string = "ATCG"
+    sequence = "".join(random.choice(string) for _ in range(length))
+    return sequence
+
+sequence = random_sequence(50)
+print(sequence)
+height = [0] * 4
+for c in sequence:
+    if c =="A":
+        height[0] += 1
+    elif c =="T":
+        height[1] += 1
+    elif c =="C":
+        height[2] += 1
+    elif c =="G":
+        height[3] += 1
+
+print(height)
