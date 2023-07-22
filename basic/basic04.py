724c651845043bc8805635965249c8b5c8544acc
I-Kouta <kouta.baseball0222@gmail.com>
2023-07-22 19:08:27 +0900

    オブジェクト作成

---
+++ basic/basic04.py
@@ -0,0 +1,12 @@
+# 185,クラス定義から(python basic04.py)
+class HelloFile:
+    def __init__(self, n): # 初期化のメソッド
+        self.n = n # オブジェkとのnという名前の属性を表す(右辺はメソッドの引数)
+
+    def readline(self):
+        if self.n == 0:
+            return ""
+        self.n = self.n - 1
+        return "Hello.\n"
+f = HelloFile(3) # オブジェクト作成, 3がnに渡される
+print(f.readline())
