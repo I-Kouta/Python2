d4aa24df1b681fc200fc50ca39e064cebd9455ff
I-Kouta <kouta.baseball0222@gmail.com>
2023-07-24 14:34:16 +0900

    文字列とリストで分ける

--- basic/basic06.py
+++ basic/basic06.py
@@ -1,5 +1,4 @@
 # pythonを用いた科学技術計算(python basic06.py)
-
 # ループの用法, 値のリストから最小値を見つける
 smallest = None
 print(f"before:{smallest}")
@@ -7,3 +6,12 @@ for itervar in [41, 12, 9, 3, 74, 15]:
     if smallest is None or itervar < smallest:
         smallest = itervar
 print(f"smallest: {smallest}")
+
+# 文字列路リスト
+words = 'His e-mail is q-lar@freecodecamp.org'
+pieces = words.split() # スペースでリストを分ける
+print(pieces)
+parts = pieces[3].split('-') # リストの3番目を"-"で分ける
+print(parts)
+n = parts[1] # lar@freecodecamp.org
+print(n)
