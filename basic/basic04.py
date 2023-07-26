7e274ca3ce0e4c8c1ac5168f493048f51a8f4349
I-Kouta <kouta.baseball0222@gmail.com>
2023-07-26 14:49:10 +0900

    再帰関数の例

--- basic/basic04.py
+++ basic/basic04.py
@@ -10,3 +10,12 @@ class HelloFile:
         return "Hello.\n"
 f = HelloFile(3) # オブジェクト作成, 3がnに渡される
 print(f.readline())
+
+# 224, 再帰関数の例
+def prefixes(s):
+    if s == "":
+        return []
+    else:
+        return [s] + prefixes(s[:-1]) # 接頭辞リストを返す
+        # return [s] + prefixes(s[1:]) # 接尾辞リストを返す
+print(prefixes("abcde"))
