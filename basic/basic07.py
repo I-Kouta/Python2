3e3e61573b4e0094e957ad2014e60624f4b4862a
I-Kouta <kouta.baseball0222@gmail.com>
2023-07-29 12:39:15 +0900

    ヒートマップ

--- basic/basic07.py
+++ basic/basic07.py
@@ -1,4 +1,5 @@
 # 229P, 簡単なデータの可視化
+import numpy as np
 import matplotlib.pyplot as plt
 
 # 折れ線グラフ
@@ -9,13 +10,19 @@ import matplotlib.pyplot as plt
 # plt.legend()
 
 # 散布図, 要素数は全て同じ数
-x = [1, 4, 9, 8]
-y = [8, 7, 6, 9]
-plt.scatter(x, y)
+# x = [1, 4, 9, 8]
+# y = [8, 7, 6, 9]
+# plt.scatter(x, y)
 
 # 棒グラフ
 # x = [1, 2, 3, 4]
 # y = {"apple":10, "banana":30, "orange":40, "kiwi":15}
 # plt.bar(x, y.values(), tick_label=list(y.keys()))
+
+# ヒートマップ
+ary1 = np.random.rand(100)
+ary2 = ary1.reshape(10, 10)
+im = plt.imshow(ary2)
+plt.colorbar(im)
 
 plt.show()
