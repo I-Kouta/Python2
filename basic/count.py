2b5dc99fd8c6ee9ee3ea13670cbb22729fe090ee
I-Kouta <kouta.baseball0222@gmail.com>
2023-10-14 14:56:45 +0900

    出現回数から出現頻度を数えるように変える

--- basic/count.py
+++ basic/count.py
@@ -8,15 +8,18 @@ alphabet_sequence = "kokugo suugaku rika eigo buturi kagaku seibutu syakai nihon
 # アルファベットの出現回数を数える
 letter_counts = collections.Counter(alphabet_sequence)
 
+# 文中のアルファベットの総数を計算
+total_letters = sum(letter_counts.values())
+
 # アルファベットのリスト(aからzまで)
 letters = [chr(ord('a') + i) for i in range(26)]
 
-# 出現回数を格納するリスト
-count_values = [letter_counts[letter] for letter in letters]
+# 出現頻度を数える
+frequency_values = [letter_counts[letter] / total_letters for letter in letters]
 
 # グラフの描画
-plt.bar(letters, count_values)
+plt.bar(letters, frequency_values)
 plt.xlabel('Alphabet')
-plt.ylabel('Count')
+plt.ylabel('Frequency')
 plt.title('Alphabet Count in the Sequence')
 plt.show()
