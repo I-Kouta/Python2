02aafb64fa3b864023ad44d3ea8c446d3a1510d3
I-Kouta <kouta.baseball0222@gmail.com>
2023-11-05 21:18:06 +0900

    数値を変換する

---
+++ ABC/bin.py
@@ -0,0 +1,12 @@
+# 10進数を変換する(python bin.py)
+num = 255
+binary_num = format(num, 'b')
+binary_hex = format(num, 'x')
+print(f"{num}は2進数では{binary_num}")
+print(f"{num}は16進数では{binary_hex}")
+print("\n")
+
+# 2進数を10進数に変換
+num_bin = "11111110"
+decimal_num = int(num_bin, 2)
+print(f"{num_bin}は10進数では{decimal_num}")
