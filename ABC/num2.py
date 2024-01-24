8ce3785aad308959dc0a313e570c9f226f1ea61e
I-Kouta <kouta.baseball0222@gmail.com>
2024-01-24 14:43:03 +0900

    16進数を10進数に変換する

--- ABC/num2.py
+++ ABC/num2.py
@@ -3,11 +3,15 @@
 
 # 10進数を16進数に変換する
 a = 255
-
 # 解法1: hexを使う。出力結果に16進数を表す「0x」が先頭に付く
 hex = hex(a)
 print(f"{a}をhexを使って16進数に変換すると「{hex}」")
-
 # 解法2: formatを使う。「0x」は付かない
 format = format(a,"x")
-print(f"{a}を1formatを使って6進数に変換すると「{format}」")
+print(f"{a}を1formatを使って6進数に変換すると「{format}」\n")
+
+# 16進数を10進数に変換する
+b ="0xf0" # 0xは無くても良い
+# intを使う
+int = int(b, 16)
+print(f"{b}を10進数に変換すると「{int}」\n")
