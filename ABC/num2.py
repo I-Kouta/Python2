8895680a2d7aaba3f11ca460bb3d5e2fd83638b7
I-Kouta <kouta.baseball0222@gmail.com>
2024-01-24 15:20:38 +0900

    流れの整理

--- ABC/num2.py
+++ ABC/num2.py
@@ -1,25 +1,29 @@
 # 2進数, 10進数, 16進数を変換する
 # python num2.py
 
-# 10進数を16進数に変換する
-a = 255
-# 解法1: hexを使うと出力結果に16進数を表す「0x」が先頭に付く
-hex = hex(a)
-print(f"{a}をhexを使って16進数に変換すると「{hex}」")
-# 解法2: formatを使うと「0x」は付かない
-formatHex = format(a,"x")
-print(f"{a}をformatを使って6進数に変換すると「{formatHex}」\n")
+# 16進数を10進数に変換する
+a ="0xff" # 0xは無くても良い
+# intを使う
+intDecimal = int(a, 16) # ここで16進数が10進数に変換される
+print(f"{a}を10進数に変換すると「{intDecimal}」\n")
 
 # 10進数を2進数に変換する
-bin = bin(a)
+changeBin = bin(intDecimal)
 # 解法1: binを使うと出力結果に2進数を表す「0b」が先頭に付く
-print(f"{a}をbinを使って2進数に変換すると「{bin}」")
+print(f"{intDecimal}をbinを使って2進数に変換すると「{changeBin}」")
 # 解法2: formatを使うと「0b」は付かない
-formatBin = format(a,"b")
-print(f"{a}をformatを使って6進数に変換すると「{formatBin}」\n")
+formatBin = format(intDecimal,"b")
+print(f"{intDecimal}をformatを使って2進数に変換すると「{formatBin}」\n")
 
-# 16進数を10進数に変換する
-b ="0xf0" # 0xは無くても良い
-# intを使う
-int = int(b, 16)
-print(f"{b}を10進数に変換すると「{int}」\n")
+# 2進数を10進数に変換する
+binary_num = "11111100"
+intBin = int(binary_num, 2)
+print(f"{binary_num}をformatを使って16進数に変換すると「{intBin}」\n")
+
+# 10進数を16進数に変換する
+# 解法1: hexを使うと出力結果に16進数を表す「0x」が先頭に付く
+hex = hex(intBin)
+print(f"{intBin}をhexを使って16進数に変換すると「{hex}」")
+# 解法2: formatを使うと「0x」は付かない
+formatHex = format(intBin,"x")
+print(f"{intBin}をformatを使って16進数に変換すると「{formatHex}」\n")
