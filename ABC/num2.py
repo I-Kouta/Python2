77848d8157d732da355667cde92f125d6135dc43
I-Kouta <kouta.baseball0222@gmail.com>
2024-01-24 14:49:03 +0900

    10進数を2進数に変換する

--- ABC/num2.py
+++ ABC/num2.py
@@ -3,12 +3,20 @@
 
 # 10進数を16進数に変換する
 a = 255
-# 解法1: hexを使う。出力結果に16進数を表す「0x」が先頭に付く
+# 解法1: hexを使うと出力結果に16進数を表す「0x」が先頭に付く
 hex = hex(a)
 print(f"{a}をhexを使って16進数に変換すると「{hex}」")
-# 解法2: formatを使う。「0x」は付かない
-format = format(a,"x")
-print(f"{a}を1formatを使って6進数に変換すると「{format}」\n")
+# 解法2: formatを使うと「0x」は付かない
+formatHex = format(a,"x")
+print(f"{a}をformatを使って6進数に変換すると「{formatHex}」\n")
+
+# 10進数を2進数に変換する
+bin = bin(a)
+# 解法1: binを使うと出力結果に2進数を表す「0b」が先頭に付く
+print(f"{a}をbinを使って2進数に変換すると「{bin}」")
+# 解法2: formatを使うと「0b」は付かない
+formatBin = format(a,"b")
+print(f"{a}をformatを使って6進数に変換すると「{formatBin}」\n")
 
 # 16進数を10進数に変換する
 b ="0xf0" # 0xは無くても良い
