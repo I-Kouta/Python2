6b5889f31b148175c3d4556fb28f904c312f0c70
I-Kouta <kouta.baseball0222@gmail.com>
2024-03-12 20:13:59 +0900

    試し割り法

---
+++ numpy/findPrimeNumbers.py
@@ -0,0 +1,20 @@
+# 試し割り法のサンプルコード
+# python findPrimeNumbers.py
+
+def findPrimeNumbers(n):
+    a = []
+    while n % 2 == 0:
+        a.append(2)
+        n //= 2
+    f = 3
+    while f * f <= n:
+        if n % f == 0:
+            a.append(f)
+            n //= f
+        else:
+            f += 2
+    if n != 1:
+        a.append(n)
+    return a
+
+print(findPrimeNumbers(36))
