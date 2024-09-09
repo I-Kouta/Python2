e45804eb36cc1ac50d8f5f44614f040b3923c394
I-Kouta <kouta.baseball0222@gmail.com>
2024-09-09 15:53:52 +0900

    浮動小数点、複素数

--- Python-izm/基礎編/integer.py
+++ Python-izm/基礎編/integer.py
@@ -1,4 +1,15 @@
 # python integer.py
+
 string = "100"
 result = int(string) + 300 # 文字を数値に変換している
 print("100 + 300 = " + str(result)) # 数値を文字に変換して結合している
+
+# 浮動小数点
+num = "100.5"
+result = float(num) + .5 # 整数部分が0なら省略可
+print(str(num) + " + 0.5 = " + str(result))
+
+# 複素数
+complex = 100 + 5j
+print(complex.real) # 実部
+print(complex.imag) # 虚部
