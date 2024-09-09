f5a7cbcb0f1fd5b179bb19192f3971097ed20b8c
I-Kouta <kouta.baseball0222@gmail.com>
2024-09-09 13:50:49 +0900

    文字列

--- Python-izm/基礎編/string.py
+++ Python-izm/基礎編/string.py
@@ -1,6 +1,19 @@
 # python string.py
 
 # 文字列への追加
+# クォート3つで複数行に分けられる
+text = """
+1行目
+2行目
+"""
+print(text)
+# 文字列へ変換、置換
+integer = 100
+print(str(integer) + "円")
+print(str(integer).replace("100", "200") + "円")
+# 文字列の連結、分割
 test_str = "123"
-test_str += "456"
+test_str += "-456"
+test_str += "-789" * 3
 print(test_str)
+print(test_str.split("-"))
