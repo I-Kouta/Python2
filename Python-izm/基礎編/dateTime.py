07ce0a809e1aed4ab614c87ab7d76a2a34c55768
I-Kouta <kouta.baseball0222@gmail.com>
2024-09-10 16:40:53 +0900

    日付の計算

--- Python-izm/基礎編/dateTime.py
+++ Python-izm/基礎編/dateTime.py
@@ -1,5 +1,6 @@
 # python dateTime.py
 import datetime
+import calendar
 
 today = datetime.date.today()
 todayDetail = datetime.datetime.today()
@@ -16,4 +17,15 @@ print(todayDetail.minute)
 print(todayDetail.second, "\n")
 # 日付のフォーマット
 print(today.isoformat()) # 年 - 月 - 日
-print(todayDetail.strftime("%Y/%M/%D %H:%M:%S")) # 年 - 月 - 日 時:分:秒
+print(todayDetail.strftime("%Y/%M/%D %H:%M:%S"), "\n") # 年 - 月 - 日 時:分:秒
+
+# 日付の計算
+print(todayDetail) # 今日
+print(todayDetail + datetime.timedelta(days = 1)) # 翌日
+newYear = datetime.datetime(2024, 1, 1)
+calc = todayDetail - newYear
+print("今日は1/1から" + str(calc.days) + "日後", "\n")
+#うるう年の判定
+print(calendar.isleap(2015)) # false
+print(calendar.isleap(2024)) # true
+print(calendar.leapdays(2010, 2020)) # して期間内にうるう年がある回数
