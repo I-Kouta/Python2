35b70c26708063d8089dd38b40dab84b816d2a85
I-Kouta <kouta.baseball0222@gmail.com>
2024-09-10 23:52:49 +0900

    リストの修飾

--- Python-izm/基礎編/tuple.py
+++ Python-izm/基礎編/tuple.py
@@ -13,9 +13,11 @@ print(testTuple[1]) # 月だけ
 print(testTuple[2]) # 日だけ
 
 # リスト
-testList = ["111", "222", "333"]
+testList = ["111", "111", "1111"]
 
 testList.insert(1, "-") # インデックス値を指定して要素を追加(appendだと末尾に付与)
 testList.insert(3, "-")
 testList.pop(4) # 該当のインデックス値の要素を削除
 print(testList)
+print(testList.index("111")) # 引数に該当するインデックス値を返す
+print(testList.count("111")) # 指定の引数の数を数える
