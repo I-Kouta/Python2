725c4a4b4aa7216710083c26f72cb15a96ae4de0
I-Kouta <kouta.baseball0222@gmail.com>
2024-09-12 00:30:45 +0900

    リスト

--- Python-izm/基礎編/dict.py
+++ Python-izm/基礎編/dict.py
@@ -5,3 +5,9 @@ print(testDict) # ただ出力されるだけ
 for i in testDict:
     print(i)
     print(testDict[i])
+
+# リスト
+testList =["123", "456", "789", "012"]
+print(testList[-1:]) # 末尾の1つ
+print(testList[:-1]) # 末尾の1つ以外
+print(testList[::-1]) # 全ての逆順
