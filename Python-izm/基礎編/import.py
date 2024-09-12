34ed4c4bc2ed9087ba0962164fe060ae995d36d0
I-Kouta <kouta.baseball0222@gmail.com>
2024-09-12 14:00:23 +0900

    説明追加

--- Python-izm/基礎編/import.py
+++ Python-izm/基礎編/import.py
@@ -1,11 +1,11 @@
 # python import.py
 
-import testmod
+import testmod # 対象のモジュールのインポート
 
-test_class_1 = testmod.TestClass()
-test_class_1.test_method('1')
+test_class_1 = testmod.TestClass() # インスタンスの作成
+test_class_1.test_method('1') # 呼び出し
 
 from testmod import TestClass
 
-test_class_2 = testmod.TestClass()
+test_class_2 = TestClass()
 test_class_2.test_method('2')
