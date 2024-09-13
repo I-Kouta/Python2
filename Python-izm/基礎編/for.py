ffc4cd86d8a97d05d52b761b7e9632fa1aa784ce
I-Kouta <kouta.baseball0222@gmail.com>
2024-09-13 20:57:15 +0900

    for文

---
+++ Python-izm/基礎編/for.py
@@ -0,0 +1,14 @@
+# python for.py
+
+sample = []
+sample.append("1つめ")
+sample.append("---")
+sample.append("2つめ")
+sample.append("---")
+sample.append("3つめ")
+sample.append("---")
+
+for val in sample:
+    print(val)
+
+# while文
