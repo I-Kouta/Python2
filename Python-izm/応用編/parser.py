e1573ff6eb3c0cb0b72934eab3bc0e3b4a2eba4d
I-Kouta <kouta.baseball0222@gmail.com>
2024-09-15 19:02:08 +0900

    HTML解析

---
+++ Python-izm/応用編/parser.py
@@ -0,0 +1,10 @@
+# python parser.py
+# HTML解析
+import urllib.request
+
+url = 'http://www.python-izm.com/'
+
+htmldata = urllib.request.urlopen(url)
+print(htmldata.read().decode('UTF-8'))
+
+htmldata.close()
