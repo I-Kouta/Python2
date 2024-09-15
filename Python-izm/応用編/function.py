1f552114af6edcfc0522d8db73803f2cb1b60ef3
I-Kouta <kouta.baseball0222@gmail.com>
2024-09-15 14:47:26 +0900

    クラスの作成

--- Python-izm/応用編/function.py
+++ Python-izm/応用編/function.py
@@ -11,3 +11,22 @@ def testFunc(num1, num2, num3):
         print("割り算 : " + str(num2) + " / " + str(num3) + " = " + str(num2 / num3))
 
 testFunc(5, 3, 3)
+
+print("\n")
+
+# クラス作成
+
+class TestClass:
+
+    def __init__(self, code, name): # クラスの初期化時に必ず実行される(省略できない)
+        self.code = code
+        self.name = name
+
+classes = []
+classes.append(TestClass(1, "テスト1")) # クラスのインスタンス化
+classes.append(TestClass(2, "テスト2")) # クラスのインスタンス化
+
+for test_cls in classes:
+    print("--- クラス ---")
+    print("code --> " + str(test_cls.code))
+    print("name --> " + test_cls.name)
