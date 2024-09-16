1aa985df509d53906a86949e320e80417042d668
I-Kouta <kouta.baseball0222@gmail.com>
2024-09-16 14:09:00 +0900

    UUID

--- Python-izm/応用編/hash.py
+++ Python-izm/応用編/hash.py
@@ -1,5 +1,12 @@
 # python hash.py
 import hashlib
+import uuid
 
 print(hashlib.md5(b"perfume").hexdigest())
 print(hashlib.sha256(b"perfume").hexdigest())
+
+print("\n")
+
+# UUIDの生成(ランダムな生成)
+
+print(uuid.uuid4())
