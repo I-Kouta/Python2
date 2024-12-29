b05b30bcd641b118a465e529b76566e61ab73289
I-Kouta <kouta.baseball0222@gmail.com>
2024-12-29 11:21:58 +0900

    表示位置指定

--- Python-izm/GUI/frame.py
+++ Python-izm/GUI/frame.py
@@ -6,6 +6,7 @@ import wx
 application = wx.App() # 初期化
 
 frame = wx.Frame(None, wx.ID_ANY, "テストフレーム", size = (300, 300)) # Frameを生成
+frame.Centre() # 中央に表示する場合
 frame.Show() # 可視化の処理
 
 application.MainLoop() # イベント待ち受け状態へ遷移
