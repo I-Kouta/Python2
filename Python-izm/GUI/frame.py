3df449b324d3abbadc1f658b3bf82393abebdfc4
I-Kouta <kouta.baseball0222@gmail.com>
2024-12-29 12:52:58 +0900

    パネル色分け

--- Python-izm/GUI/frame.py
+++ Python-izm/GUI/frame.py
@@ -5,8 +5,19 @@ import wx
 
 application = wx.App() # 初期化
 
-frame = wx.Frame(None, wx.ID_ANY, "テストフレーム", size = (300, 300)) # Frameを生成
+x = 300
+y = 300
+frame = wx.Frame(None, wx.ID_ANY, "テストフレーム", size = (x, y)) # Frameを生成(横, 縦)
 frame.Centre() # 中央に表示する場合
+
+r_panel = wx.Panel(frame, wx.ID_ANY, pos = (0, 0), size = (100, 300)) # Panelを生成
+r_panel.SetBackgroundColour("#ffcccc")
+
+r_panel = wx.Panel(frame, wx.ID_ANY, pos = (100, 0), size = (100, 300)) # Panelを生成
+r_panel.SetBackgroundColour("#ccffcc")
+
+r_panel = wx.Panel(frame, wx.ID_ANY, pos = (200, 0), size = (100, 300)) # Panelを生成
+r_panel.SetBackgroundColour("#ccccff")
 
 frame.CreateStatusBar()
 frame.SetStatusText("ステータスバー") # 下部にステータスバーを表示
