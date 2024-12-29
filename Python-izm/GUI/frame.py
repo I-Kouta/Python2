5b957a4db9fb4c33e6334b24f83aec77bc5dda74
I-Kouta <kouta.baseball0222@gmail.com>
2024-12-29 11:20:08 +0900

    サイズ指定

--- Python-izm/GUI/frame.py
+++ Python-izm/GUI/frame.py
@@ -3,9 +3,9 @@
 
 import wx
 
-application = wx.App()
+application = wx.App() # 初期化
 
-frame = wx.Frame(None, wx.ID_ANY, "テストフレーム")
-frame.Show()
+frame = wx.Frame(None, wx.ID_ANY, "テストフレーム", size = (300, 300)) # Frameを生成
+frame.Show() # 可視化の処理
 
-application.MainLoop()
+application.MainLoop() # イベント待ち受け状態へ遷移
