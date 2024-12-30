b107a37203cb33cf78d02213d64aff926787a257
I-Kouta <kouta.baseball0222@gmail.com>
2024-12-30 16:50:06 +0900

    前提条件

--- Python-izm/応用編/hisyo.py
+++ Python-izm/応用編/hisyo.py
@@ -1,3 +1,19 @@
 
 # 秘書問題を解く
 # python hisyo.py
+
+import numpy as np
+
+# 応募者の人数
+N = 100
+
+# シミュレーション回数
+n_exam = 1000
+
+# 評価値の最大値
+max_eval = 100
+
+# 採用結果、採用した人の評価値の履歴、できなければ0
+history = []
+
+np.random.seed(1)
