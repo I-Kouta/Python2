6c3634c318673c4122a86da9502b2087c9abf2eb
I-Kouta <kouta.baseball0222@gmail.com>
2025-01-03 13:35:08 +0900

    試行回数のエラー解消

--- Python-izm/応用編/monty-hall.py
+++ Python-izm/応用編/monty-hall.py
@@ -5,8 +5,14 @@
 import random
 
 def sim_choice():
-    num_sim = int(input("試行回数を入力してください : ")) # 試行回数入力、文字列を数字に変換
-    plyer_choice = input("ドアを変更する YES = y or NO = n") # ドア変更するか？
+    while True:
+        try:
+            num_sim = int(input("試行回数を入力してください : ")) # 試行回数入力、文字列を数字に変換
+            plyer_choice = input("ドアを変更する YES = y or NO = n") # ドア変更するか？
+        except ValueError: # 小数か文字列を入れると発生
+            print("数字を入力してください")
+        else: # 正常終了時
+            break
     if plyer_choice == "y": # ドア変更あり
         print(f"試行回数 : {num_sim}回、ドア変更あり")
     elif plyer_choice == "n": # ドア変更なし
