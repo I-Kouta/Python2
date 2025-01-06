9b0ac217f791b7b42066f4bfb4a7814795a2047b
I-Kouta <kouta.baseball0222@gmail.com>
2025-01-06 17:11:27 +0900

    ドア変更の選択肢を消す

--- Python-izm/応用編/monty-hall.py
+++ Python-izm/応用編/monty-hall.py
@@ -8,20 +8,12 @@ def sim_choice():
     while True:
         try:
             num_sim = int(input("試行回数を入力してください : ")) # 試行回数入力、文字列を数字に変換
-            plyer_choice = input("ドアを変更する YES = y or NO = n : ") # ドア変更するか？
         except ValueError: # 小数か文字列を入れると発生
             print("数字を入力してください")
         else: # 正常終了時
-            pass
-        if plyer_choice == "y": # ドア変更あり
-            print(f"試行回数 : {num_sim}回、ドア変更あり")
+            print(f"試行回数 : {num_sim}回")
             break
-        elif plyer_choice == "n": # ドア変更なし
-            print(f"試行回数 : {num_sim}回、ドア変更なし")
-            break
-        else:
-            print("yかnを入力してください")
-    return [num_sim, plyer_choice] # リストで返す[試行回数, ドア変y or n]
+    return num_sim
 
 def monty(num, y_or_n):
     sim_count = 0 # これまでの試行回数
