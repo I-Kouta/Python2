7b15838550bad523f24d90d9dbe254222afb9010
I-Kouta <kouta.baseball0222@gmail.com>
2025-01-07 12:51:59 +0900

    10回繰り返す

--- Python-izm/応用編/lots.py
+++ Python-izm/応用編/lots.py
@@ -7,9 +7,16 @@ import random
 # くじ作成
 lot = ["〇", "×1", "×2","×3","×4","×5","×6","×7","×8","×9",]
 
+# くじを引いた回数
+lot_result = 0
 # くじを引く
-your_lot = random.choice(lot)
-if your_lot == "〇":
-    print(f"{your_lot}, あたり")
-else:
-    print(f"{your_lot}, はずれ")
+while True:
+    if lot_result == 10:
+        break
+    else:
+        your_lot = random.choice(lot)
+        lot_result += 1
+        if your_lot == "〇":
+            print(f"{your_lot}, あたり")
+        else:
+            print(f"{your_lot}, はずれ")
