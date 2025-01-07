c28b23024333044b7f49f2a6ca0ae6fa5dbb05cf
I-Kouta <kouta.baseball0222@gmail.com>
2025-01-07 12:42:10 +0900

    くじを引いてみる

--- Python-izm/応用編/lots.py
+++ Python-izm/応用編/lots.py
@@ -1,3 +1,15 @@
 
 # 確率の悪魔
 # python lots.py
+
+import random
+
+# くじ作成
+lot = ["〇", "×1", "×2","×3","×4","×5","×6","×7","×8","×9",]
+
+# くじを引く
+your_lot = random.choice(lot)
+if your_lot == "〇":
+    print(f"{your_lot}, あたり")
+else:
+    print(f"{your_lot}, はずれ")
