3b46d73c1c211998426dcf2d0f846e070d9e3797
I-Kouta <kouta.baseball0222@gmail.com>
2025-01-08 12:07:28 +0900

    回数指定

--- Python-izm/応用編/lots.py
+++ Python-izm/応用編/lots.py
@@ -8,15 +8,25 @@ import random
 lot = ["〇", "×1", "×2","×3","×4","×5","×6","×7","×8","×9",]
 
 # くじを引いた回数
-lot_result = 0
+lot_num = 0
+# あたりの回数
+win = 0
 # くじを引く
 while True:
-    if lot_result == 10:
+    if lot_num == 10:
         break
     else:
         your_lot = random.choice(lot)
-        lot_result += 1
+        lot_num += 1
         if your_lot == "〇":
-            print(f"{your_lot}, あたり")
+            print(f"{lot_num}回目、{your_lot}, あたり")
+            win += 1
         else:
-            print(f"{your_lot}, はずれ")
+            print(f"{lot_num}回目、{your_lot}, はずれ")
+            pass
+
+# 10回であたったか判定
+if win == 0:
+    print("当たりなし")
+else:
+    print(f"当たり{win}回")
