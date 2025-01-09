ef642d377f28545061086e8ef74ae552192f44d0
I-Kouta <kouta.baseball0222@gmail.com>
2025-01-09 11:59:11 +0900

    まだうまくいっていない

--- Python-izm/応用編/lots.py
+++ Python-izm/応用編/lots.py
@@ -7,16 +7,16 @@ import random
 # くじ作成
 lot = ["〇", "×1", "×2","×3","×4","×5","×6","×7","×8","×9",]
 lot_num = 0 # くじを引いた回数
-win = 0 # あたりの回数
+win_count= 0 # あたりの回数
 
 prog_num = 0 # 10回判定のプログラムの試行回数
 prog_win_num = 0 # 10回判定のプログラムのあたり回数
 
-# くじを引く
+# 10回繰り返す
 while True:
     if prog_num == 10:
         break
-    else:
+    else: # くじを引く
         while True:
             if lot_num == 10:
                 prog_num += 1
@@ -25,14 +25,17 @@ while True:
                 your_lot = random.choice(lot)
                 lot_num += 1
                 if your_lot == "〇":
-                    print(f"{lot_num}回目、{your_lot}, あたり")
-                    win += 1
+                    # print(f"{lot_num}回目、{your_lot}, あたり")
+                    win_count+= 1
                 else:
-                    print(f"{lot_num}回目、{your_lot}, はずれ")
+                    # print(f"{lot_num}回目、{your_lot}, はずれ")
                     pass
-
-# 10回であたったか判定
-if win == 0:
-    print("当たりなし")
-else:
-    print(f"当たり{win}回")
+        if prog_win_num == 0:
+            print("あたりなし")
+        else:
+            print(f"{prog_win_num}回目にあたり")
+            prog_win_num += 1
+        lot_num = 0
+        win_count= 0
+print("試行回数", prog_num)
+print("あたり回数", prog_win_num)
