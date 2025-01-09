749311040d5867a4ef2a7298a19daab763458fa4
I-Kouta <kouta.baseball0222@gmail.com>
2025-01-09 11:50:28 +0900

    10回試行する

--- Python-izm/応用編/lots.py
+++ Python-izm/応用編/lots.py
@@ -6,24 +6,30 @@ import random
 
 # くじ作成
 lot = ["〇", "×1", "×2","×3","×4","×5","×6","×7","×8","×9",]
+lot_num = 0 # くじを引いた回数
+win = 0 # あたりの回数
 
-# くじを引いた回数
-lot_num = 0
-# あたりの回数
-win = 0
+prog_num = 0 # 10回判定のプログラムの試行回数
+prog_win_num = 0 # 10回判定のプログラムのあたり回数
+
 # くじを引く
 while True:
-    if lot_num == 10:
+    if prog_num == 10:
         break
     else:
-        your_lot = random.choice(lot)
-        lot_num += 1
-        if your_lot == "〇":
-            print(f"{lot_num}回目、{your_lot}, あたり")
-            win += 1
-        else:
-            print(f"{lot_num}回目、{your_lot}, はずれ")
-            pass
+        while True:
+            if lot_num == 10:
+                prog_num += 1
+                break
+            else:
+                your_lot = random.choice(lot)
+                lot_num += 1
+                if your_lot == "〇":
+                    print(f"{lot_num}回目、{your_lot}, あたり")
+                    win += 1
+                else:
+                    print(f"{lot_num}回目、{your_lot}, はずれ")
+                    pass
 
 # 10回であたったか判定
 if win == 0:
