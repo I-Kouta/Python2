b77495d1cc6d3a755855d6520b3da5018a0ae5fe
I-Kouta <kouta.baseball0222@gmail.com>
2025-01-10 14:32:43 +0900

    時間計測

--- Python-izm/応用編/time.py
+++ Python-izm/応用編/time.py
@@ -1,3 +1,12 @@
 
 # ストップウォッチをつくる
 # python time.py
+
+import time
+
+start_signal = input("Push 'ENTER' to Start")
+start_time = time.time()
+stop_signal = input("Push 'ENTER' to Stop")
+stop_time = time.time()
+result = stop_time - start_time
+print(f"Stop Time : {result:.3f}s")
