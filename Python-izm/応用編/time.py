4252249d7ba79719d6cd434c8c677061ea2bf0a1
I-Kouta <kouta.baseball0222@gmail.com>
2025-01-10 17:51:13 +0900

    時間表示の変換

--- Python-izm/応用編/time.py
+++ Python-izm/応用編/time.py
@@ -4,9 +4,18 @@
 
 import time
 
+def convert(sec):
+    minutes = sec // 60 # 分の部分
+    second = sec % 60 # 秒の部分
+    milli_sec = (second - int(second)) * 1000
+    hour = minutes // 60
+    min = minutes % 60
+    return f"{int(hour)} : {int(min)} : {int(second)} : {int(milli_sec)}"
+
 start_signal = input("Push 'ENTER' to Start")
 start_time = time.time()
 stop_signal = input("Push 'ENTER' to Stop")
 stop_time = time.time()
 result = stop_time - start_time
-print(f"Stop Time : {result:.3f}s")
+time_result = convert(result)
+print(f"Stop Time : {time_result}")
