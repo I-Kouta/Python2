45c6028a8f3bb1c750c4a2415972cc0eff0e9634
I-Kouta <kouta.baseball0222@gmail.com>
2025-01-16 12:01:36 +0900

    動的計画法

--- Python-izm/応用編/score.py
+++ Python-izm/応用編/score.py
@@ -11,7 +11,7 @@ from fractions import Fraction
 
 # 2.n = 4とする時、得点が0でない確率・25である確率
 n = 4
-iteration = 10000 # 試行回数
+iteration = 100000 # 試行回数
 count1 = 0 # 得点が0でない回数
 count2 = 0 # 得点が25の回数
 
@@ -37,3 +37,41 @@ for _ in range(iteration):
 
 print(f"得点が0でない確率 : {count1 / iteration} = {Fraction(count1 / iteration).limit_denominator(100)}") # 約0.375(3 / 8)
 print(f"得点が25の確率 : {count2 / iteration} = {Fraction(count2 / iteration).limit_denominator(100)}") # 約0.125(1 / 8)
+print("-----")
+
+# 3.n = 9とする時、得点が100である確率・奇数である確率
+n = 9
+
+# i回コインを投げて、石の位置がjで得点がkである確率
+dp = [[[0] * (200) for _ in range(50)] for _ in range(n + 1)]
+
+# 最初は石の位置が0で、得点が0
+dp[0][0][0] = 1
+
+for i in range(n):
+    for j in range(3*(i+1)+1):
+        for k in range(3*n*(n+1)//2+1):
+            # i回コインを投げて、石の位置がjであって、得点がkである場合からの遷移を考える
+            # i + 1回目のコイン投げで表が出た場合、
+            # 石の位置は2進み、得点はその時点での石の位置であるj + 2が可算される
+            dp[i + 1][j + 2][k + j + 2] += dp[i][j][k] * (1 / 2)
+
+            # i+1回目のコイン投げで裏が出た場合,
+            # 石の位置は3進み、得点はその時点での石の位置である j+3 が可算される
+            dp[i + 1][j + 3][k + j + 3] += dp[i][j][k] * (1 / 2)
+
+count1 = 0  # 得点が100である確率
+count2 = 0  # 得点が奇数である確率
+
+for j in range(3 * n + 1):
+    for k in range(3 * n * (n + 1) // 2 + 1):
+
+        if j == 2 * n + 2 and k == 100: # 得点が100であるときをカウント
+            count1 += dp[n][j][k]
+
+        if j == 2 * n + 2 and k % 2 == 1: # 得点が奇数であるときをカウント
+            count2 += dp[n][j][k]
+
+
+print(f"得点が100である確率 : {count1} = {Fraction(count1).limit_denominator(1000)}") # 約1 / 128
+print(f"得点が奇数である確率 : {count2} = {Fraction(count2).limit_denominator(1000)}") # 約5 / 128
