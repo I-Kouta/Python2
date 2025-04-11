86315039f07fa356a3cd3f38ef9ef8240abd50fe
I-Kouta <kouta.baseball0222@gmail.com>
2025-04-12 07:49:37 +0900

    自動化

--- numpy/pie.py
+++ numpy/pie.py
@@ -5,7 +5,7 @@ import numpy as np
 NUM = 5000 # 実行回数
 x = 0 # 点のx値
 y = 0 # 点のy値
-pai=0.0 # 求めた円周率を格納
+pai = 0.0 # 求めた円周率を格納
 a = 0 # 1/4の円に入ってる点の数をカウント
 i = 0 # ループカウンタ
 while i < NUM:
@@ -13,9 +13,9 @@ while i < NUM:
     y = np.random.rand() # ランダムな点の位置y
     if x ** 2 + y ** 2 <= 1: # 原点との距離が1未満なら
         a += 1 # 1/4の円の中なのでカウント
-        plt.scatter(x, y, color = 'red') # 赤い点で書く
+        plt.scatter(x, y, color = "red") # 赤い点で書く
     else: # 円の外であれば
-        plt.scatter(x, y, color = 'blue') # 青い点で書く
+        plt.scatter(x, y, color = "blue") # 青い点で書く
     i += 1 # ループカウンタ増
 pai = 4 * a / NUM # 円周率を求める式
 plt.title(f"pie = {pai}")
