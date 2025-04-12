5491ee3777f92c8ca631e409ba6821b40fc6102d
I-Kouta <kouta.baseball0222@gmail.com>
2025-04-12 11:54:19 +0900

    ウィンドウサイズ指定

--- autoSushida/main.py
+++ autoSushida/main.py
@@ -5,6 +5,10 @@ from selenium import webdriver
 # ドライバーを開く
 driver = webdriver.Chrome()
 
+# ウィンドウサイズを指定
+window = (500, 420 + 123)
+driver.set_window_size(*window)
+
 target_url = "https://sushida.net/"
 driver.get(target_url)
 
@@ -12,3 +16,4 @@ input("何か入力してください")
 
 # ドライバーを閉じる
 driver.close()
+driver.quit() # ターミナルでEnterで閉じる
