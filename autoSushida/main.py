4c16e6b0d34a8a71c13185d86b1c70c31781967a
I-Kouta <kouta.baseball0222@gmail.com>
2025-04-12 12:32:22 +0900

    画面はずれていない

--- autoSushida/main.py
+++ autoSushida/main.py
@@ -12,6 +12,12 @@ driver.set_window_size(*window)
 target_url = "https://sushida.net/"
 driver.get(target_url)
 
+# ゲーム画面をずらすための記載
+target_xpath = '//*[@id="game"]/div'
+webgl_element = driver.find_element_by_xpath(target_xpath)
+actions = ActionChains(driver)
+actions.move_to_element(webgl_element).perform()
+
 input("何か入力してください")
 
 # ドライバーを閉じる
