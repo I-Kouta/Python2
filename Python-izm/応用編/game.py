6634fa1af97573a9403679b177c80b6282b34e23
I-Kouta <kouta.baseball0222@gmail.com>
2025-08-31 16:11:43 +0900

    1ケースを想定

--- Python-izm/応用編/game.py
+++ Python-izm/応用編/game.py
@@ -4,18 +4,6 @@
 
 import random
 
-# 利得表を辞書で表現
-payoff_matrix = {
-    ("黙秘", "黙秘") : (0, 0),
-    ("黙秘", "自白") : (-3, 0),
-    ("自白", "黙秘") : (0, -3),
-    ("自白", "自白") : (-2, -2)
-}
-
-print(f'どちらも自白の場合：{payoff_matrix[("自白", "自白")]}')
-print(f'片方が自白の場合：{payoff_matrix[("黙秘", "自白")]}, {payoff_matrix[("自白", "黙秘")]}')
-print(f'どちらも黙秘の場合：{payoff_matrix[("黙秘", "黙秘")]}')
-
 def always_silent(): # 常に黙秘する
     return "黙秘"
 
@@ -24,3 +12,21 @@ def always_betray(): # 常に自白する
 
 def random_choice(): # ランダムに選択する
     return random.choice(["黙秘", "自白"])
+
+# 利得表
+payoff_matrix = {
+    ("黙秘", "黙秘") : (-1, -1),
+    ("黙秘", "自白") : (-3, 0),
+    ("自白", "黙秘") : (0, -3),
+    ("自白", "自白") : (-2, -2)
+}
+
+def play_games(strategy_A, strategy_B):
+    choice_A = strategy_A() # 選択した戦略
+    choice_B = strategy_B() # 選択した戦略
+    payoff_A, payoff_B = payoff_matrix[(choice_A, choice_B)]
+
+    print(f"A : {choice_A}, B : {choice_B} → 利得 : A = {payoff_A}, B = {payoff_B}")
+    return payoff_A, payoff_B
+
+play_games(always_silent, always_betray) # Aは常に黙秘、Bは常に自白
