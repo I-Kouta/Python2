5b59a2cc8a7e7618e0d5d835a17ddcf29872bd9f
I-Kouta <kouta.baseball0222@gmail.com>
2026-09-16 11:35:24 +0900

    計算速度比較

--- DP/dp.py
+++ DP/dp.py
@@ -1,6 +1,7 @@
 
 # 動的計画法
 # python dp.py
+import timeit
 
 # DPなし フィボナッチ数列
 # 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, ...
@@ -10,7 +11,7 @@ def fib(n):
     if n == 1:
         return 1
     return fib(n-1) + fib(n-2)
-print(fib(9))
+print(fib(10))
 
 # DPあり フィボナッチ数列
 def fib_DP(n, memo={}):
@@ -23,4 +24,25 @@ def fib_DP(n, memo={}):
 
     memo[n]=fib_DP(n-1, memo) + fib_DP(n-2, memo)
     return memo[n]
-print(fib_DP(9))
+print(fib_DP(10))
+
+# 計算速度を比較
+ns = [5, 20, 35]
+results = []
+
+# 各項の計算時間を測定
+for n in ns:
+    fib_result = fib(n)
+    fib_DP_result = fib_DP(n, {})
+    # 実行時間を計測
+    original_time = timeit.timeit('fib(n)', globals = globals(), number = 1)
+    memoized_time = timeit.timeit('fib_DP(n)', globals = globals(), number = 1)
+    results.append((n, fib_result, original_time, fib_DP_result, memoized_time))
+
+# 結果を表示
+for result in results:
+    n, fib_result, original_time, fib_DP_result, memoized_time = result
+    print(f"n = {n}")
+    print(f"動的計画法なし: 解 {fib_result}  実行時間 {original_time:.6f} s")
+    print(f"動的計画法あり: 解 {fib_DP_result}  実行時間 {memoized_time:.6f} s")
+    print()
